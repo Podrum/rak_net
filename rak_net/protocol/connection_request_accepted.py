4d85c3d87b4b09bf527a99c269a2ab6fead1cb8f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-28 20:41:54 +0300

    Create connection_request_accepted.py

---
+++ rak_net/protocol/connection_request_accepted.py
@@ -0,0 +1,55 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from rak_net.constant.protocol_info import protocol_info
+from rak_net.protocol.packet import packet
+
+class connection_request_accepted(packet):
+    def __init__(self, data: bytes = b"", pos: int = 0):
+        super().__init__(data, pos)
+        self.packet_id: int = protocol_info.connection_request_accepted
+  
+    def decode_payload(self) -> None:
+        self.client_address: object = self.read_address()
+        self.system_index: int = self.read_unsigned_short_be()
+        self.system_addresses: list = []
+        for i in range(0, 20):
+            self.system_addresses.append(self.read_address())
+        self.request_timestamp: int = self.read_unsigned_long_be()
+        self.accepted_timestamp: int = self.read_unsigned_long_be()
+        
+    def encode_payload(self) -> None:
+        self.write_address(self.client_address)
+        self.write_unsigned_short_be(self.system_index)
+        for address in self.system_addresses:
+            self.write_address(address)
+        self.write_unsigned_long_be(self.request_timestamp)
+        self.write_unsigned_long_be(self.accepted_timestamp)
