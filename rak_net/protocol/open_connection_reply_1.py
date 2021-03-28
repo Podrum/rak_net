ee1a9ffb33c6897bd6884761706b7b5bd3388a96
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-28 19:20:26 +0300

    Create open_connection_reply_1.py

---
+++ rak_net/protocol/open_connection_reply_1.py
@@ -0,0 +1,50 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from rak_net.constant.protocol_info import protocol_info
+from rak_net.protocol.packet import packet
+
+class open_connection_reply_1(packet):
+    def __init__(self, data: bytes = b"", pos: int = 0):
+        super().__init__(data, pos)
+        self.packet_id: int = protocol_info.open_connection_reply_1
+  
+    def decode_payload(self) -> None:
+        self.magic: bytes = self.read(16)
+        self.server_guid: int = self.read_unsigned_long_be()
+        self.use_security: bool = self.read_bool()
+        self.mtu_size: int = self.read_unsigned_short_be()
+        
+    def encode_payload(self) -> None:
+        self.write(self.magic)
+        self.write_unsigned_long_be(self.server_guid)
+        self.write_bool(self.use_security)
+        self.write_unsigned_short_be(self.mtu_size)
