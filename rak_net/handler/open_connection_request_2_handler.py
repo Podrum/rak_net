62e7349a0c9a830ea4a473b81304b5c1b79f7e3b
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-28 22:25:40 +0300

    Create open_connection_request_2_handler.py

---
+++ rak_net/handler/open_connection_request_2_handler.py
@@ -0,0 +1,48 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+from rak_net.constant.protocol_info import protocol_info
+from rak_net.protocol.incompatible_protocol_version import incompatible_protocol_version
+from rak_net.protocol.open_connection_reply_2 import open_connection_reply_2
+from rak_net.protocol.open_connection_request_2 import open_connection_request_2
+
+class open_connection_request_2_handler:
+    def handle(data: bytes, address: object, server: object) -> bytes:
+        packet: object = open_connection_request_2(data)
+        packet.decode()
+        new_packet: object = open_connection_reply_2()
+        new_packet.magic: bytes = protocol_info.magic
+        new_packet.server_guid: int = server.guid
+        new_packet.client_address: object = address
+        new_packet.mtu_size: int = packet.mtu_size
+        new_packet.use_encryption: bool = False
+        new_packet.encode()
+        return new_packet.data
