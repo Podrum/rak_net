c62862eb2d5a8021dedb8dcd4f46df1d7f18080f
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-04-01 00:29:20 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,57 @@
+################################################################################
+#                                                                              #
+#  ____           _                                                            #
+# |  _ \ ___   __| |_ __ _   _ _ __ ___                                        #
+# | |_) / _ \ / _` | '__| | | | '_ ` _ \                                       #
+# |  __/ (_) | (_| | |  | |_| | | | | | |                                      #
+# |_|   \___/ \__,_|_|   \__,_|_| |_| |_|                                      #
+#                                                                              #
+# Copyright 2021 Podrum Studios                                                #
+#                                                                              #
+# Permission is hereby granted, free of charge, to any person                  #
+# obtaining a copy of this software and associated documentation               #
+# files (the "Software"), to deal in the Software without restriction,         #
+# including without limitation the rights to use, copy, modify, merge,         #
+# publish, distribute, sublicense, and/or sell copies of the Software,         #
+# and to permit persons to whom the Software is furnished to do so,            #
+# subject to the following conditions:                                         #
+#                                                                              #
+# The above copyright notice and this permission notice shall be included      #
+# in all copies or substantial portions of the Software.                       #
+#                                                                              #
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR   #
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,     #
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE  #
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER       #
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING      #
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS #
+# IN THE SOFTWARE.                                                             #
+#                                                                              #
+################################################################################
+
+import setuptools
+
+setuptools.setup(
+    name = "rak_net",
+    packages = ["rak_net"],
+    install_requires = [
+        "binary-utils"
+    ],
+    version = "1.0",
+    license = "MIT",
+    description = "Podrum's RakNet library.",
+    author = "Podrum",
+    url = "https://github.com/Podrum/binary_utils",
+    keywords = ["raknet", "net", "rak", "python3"],
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ]
+)
