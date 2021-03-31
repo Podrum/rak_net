e7c6b620e08f95a82c8d2b5fb4a1db1f6d52cb08
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-03-31 23:56:47 +0300

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,5 +1,18 @@
 from rak_net.server import server
 
-rak_server = server("MCPE;Dedicated Server;428;1.16.210;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;", "0.0.0.0", 19132)
+rak_server: object = server("MCPE;Dedicated Server;428;1.16.210;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;", "0.0.0.0", 19132)
+
+class interface:
+    def on_frame(self, packet: object, connection: object) -> None:
+        print(hex(packet.body[0]))
+        
+    def on_disconnect(self, connection: object) -> None:
+        print(f"{connection.address.token} has disconnected.")
+        
+    def on_new_incoming_connection(self, connection: object) -> None:
+        print(f"{connection.address.token} successfully connected!")
+        
+rak_server.interface: object = interface
+    
 while True:
     rak_server.handle()
