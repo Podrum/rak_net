a567038ffb541c912c6fe1cacc64ed07c67cc72a
Halil Ibrahim Dilsiz <yknixz@gmail.com>
2021-03-31 15:32:19 +0200

    Update test_server.py

--- test_server.py
+++ test_server.py
@@ -1,5 +1,6 @@
 from rak_net.server import server
 
-rak_server = server("MCPE;Dedicated Server;428;1.16.210;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;", "0.0.0.0", 19132)
+rak_server: object = server("MCPE;Dedicated Server;428;1.16.210;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;", "0.0.0.0", 19132)
+    
 while True:
     rak_server.handle()
