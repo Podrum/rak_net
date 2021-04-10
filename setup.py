66aad39aba97b66eb6ef35f906148feecb687632
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-04-10 21:05:52 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     install_requires = [
         "binary-utils"
     ],
-    version = "1.2",
+    version = "1.3",
     license = "MIT",
     description = "Podrum's RakNet library.",
     author = "Podrum",
