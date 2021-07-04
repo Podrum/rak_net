8c655376afafcb99a7929051a3468d9f6acac4eb
Alexander Argentakis <38327951+MFDGaming@users.noreply.github.com>
2021-07-04 11:04:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     install_requires = [
         "binary-utils"
     ],
-    version = "1.7",
+    version = "1.8",
     license = "MIT",
     description = "Podrum's RakNet library.",
     author = "Podrum",
